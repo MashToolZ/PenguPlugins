c176d8b52d4c5f2ef865ee49f700773ca35abb28
Sakurasou <realmashiroshiina@gmail.com>
2023-11-06 14:17:59 +0100

    Added MinimizeSocial Plugin

---
+++ src/MinimizeSocial/index.ts
@@ -0,0 +1,34 @@
+import MTZPlugin from "../../classes/MTZPlugin"
+import Tooltip from "../../classes/Helpers/Tooltip"
+import { select, waitUntil } from "../../classes/Utils"
+
+new class extends MTZPlugin {
+
+	constructor() {
+		super("MinimizeSocial", "1.0.0", "Sakurasou")
+
+		this.addCSS("https://cdn.mashtoolz.xyz/lolclient/css/MinimizeSocial.css")
+	}
+
+	update() {
+		const hideButton = select(".action-bar-button.hide-button")
+		const socialBar = select(".lol-social-actions-bar.actions")
+		if (!hideButton && socialBar)
+			this.createHideButton(socialBar)
+	}
+
+	createHideButton(socialBar: HTMLElement) {
+		const hideButton = document.createElement("span")
+		hideButton.className = "action-bar-button hide-button"
+		hideButton.addEventListener("click", function () {
+			DataStore.set("socialVisibility", this.classList.toggle("toggled") ? "hidden" : "visible")
+			select("lol-social-roster.roster.social-ember-fade-in").style.visibility = this.classList.contains("toggled") ? "hidden" : "visible"
+		})
+
+		DataStore.get("socialVisibility") === "hidden" && hideButton.click()
+
+		socialBar.querySelector(".action-bar-button.ember-view")?.insertAdjacentElement("afterend", hideButton)
+
+		new Tooltip("top", hideButton, hideButton, "Toggle Visibility", true, 200, 300)
+	}
+}
