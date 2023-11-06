47537d318d4b72840615fd14810f1b17b6544d85
Sakurasou <realmashiroshiina@gmail.com>
2023-11-06 14:17:53 +0100

    Added AutoAccept Plugin

---
+++ src/AutoAccept/index.ts
@@ -0,0 +1,77 @@
+import MTZPlugin from "../../classes/MTZPlugin"
+import { Toggle, ToggleOptions } from "../../classes/Helpers/Toggle"
+import { select, subscribe, unsubscribe, waitUntil, sleep } from "../../classes/Utils"
+
+new class extends MTZPlugin {
+
+	private AutoAccepting = false
+	private playerResponse = null
+	private button: HTMLElement
+	private options: ToggleOptions
+
+	constructor() {
+		super("AutoAccept", "1.0.0", "Sakurasou")
+
+		this.options = {
+			parent: ".open-party-toggle",
+			name: this.name,
+			className: "auto-accept",
+			tooltip: "Automatically accept Queue Popups",
+			onEnable: () => {
+				subscribe("/lol-matchmaking/v1/ready-check", "ReadyCheck", this.onPlayerReponse)
+			},
+			onDisable: () => {
+				unsubscribe("/lol-matchmaking/v1/ready-check", "ReadyCheck")
+			}
+		}
+
+		this.addCSS("https://cdn.mashtoolz.xyz/lolclient/css/AutoAccept.css")
+
+		this.on("screen", this.onScreen.bind(this))
+		this.on("phase", this.onScreen.bind(this))
+	}
+
+	onScreen(screen: string) {
+		switch (screen) {
+			case "PARTIES": {
+				waitUntil(() => select(this.options.parent), false, 1000)
+					.then(() => {
+						if (!select(`#${this.options.name}`))
+							this.button = new Toggle(this.options)
+					})
+					.catch((err) => console.error(err))
+				break
+			}
+		}
+	}
+
+	onPhase(phase: string, lastPhase: string) {
+		switch (phase) {
+
+			case "LOBBY":
+			case "MATCHMAKING": {
+				this.playerResponse = null
+				break
+			}
+
+			case "READYCHECK": {
+				if (DataStore.get(`MTZ.${this.name}`) && !this.AutoAccepting) {
+					this.AutoAccepting = true
+					sleep(3000).then(() => {
+						if (this.playerResponse !== "None")
+							select(".ready-check-button-accept").click()
+						this.AutoAccepting = false
+					})
+				}
+				break
+			}
+		}
+	}
+
+	onPlayerReponse(message) {
+		try {
+			const status = JSON.parse(message.data)[2]?.data?.playerResponse || null
+			this.playerResponse = status
+		} catch { }
+	}
+}
