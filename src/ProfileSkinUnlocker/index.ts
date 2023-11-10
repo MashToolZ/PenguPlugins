c25877b119d9d0c7f827e5bf3944f855dff9397b
Sakurasou <realmashiroshiina@gmail.com>
2023-11-10 07:52:34 +0100

    Added ProfileSkinUnlocker

---
+++ src/ProfileSkinUnlocker/index.ts
@@ -0,0 +1,19 @@
+import MTZPlugin from "../../classes/MTZPlugin.js"
+
+new class extends MTZPlugin {
+
+	constructor() {
+		super("ProfileSkinUnlocker", "1.0.0", "Sakurasou")
+
+		Object.defineProperty(HTMLScriptElement.prototype, "src", {
+			get() {
+				return this.getAttribute("src")
+			},
+			set(value) {
+				if (value === "/fe/lol-skins-picker/rcp-fe-lol-skins-picker.js")
+					value = "https://cdn.mashtoolz.xyz/lolclient/js/rcp-fe-lol-skins-picker.js"
+				this.setAttribute("src", value)
+			}
+		})
+	}
+}
