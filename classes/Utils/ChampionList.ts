e7686727d7acb0ba2182882f131fd135c3e0c7df
Sakurasou <realmashiroshiina@gmail.com>
2023-11-10 07:51:00 +0100

    Added ChampionList with ids

---
+++ classes/Utils/ChampionList.ts
@@ -0,0 +1,662 @@
+export const ChampionList = [
+	{
+		"name": "Annie",
+		"id": 1
+	},
+	{
+		"name": "Olaf",
+		"id": 2
+	},
+	{
+		"name": "Galio",
+		"id": 3
+	},
+	{
+		"name": "Twisted Fate",
+		"id": 4
+	},
+	{
+		"name": "Xin Zhao",
+		"id": 5
+	},
+	{
+		"name": "Urgot",
+		"id": 6
+	},
+	{
+		"name": "LeBlanc",
+		"id": 7
+	},
+	{
+		"name": "Vladimir",
+		"id": 8
+	},
+	{
+		"name": "Fiddlesticks",
+		"id": 9
+	},
+	{
+		"name": "Kayle",
+		"id": 10
+	},
+	{
+		"name": "Master Yi",
+		"id": 11
+	},
+	{
+		"name": "Alistar",
+		"id": 12
+	},
+	{
+		"name": "Ryze",
+		"id": 13
+	},
+	{
+		"name": "Sion",
+		"id": 14
+	},
+	{
+		"name": "Sivir",
+		"id": 15
+	},
+	{
+		"name": "Soraka",
+		"id": 16
+	},
+	{
+		"name": "Teemo",
+		"id": 17
+	},
+	{
+		"name": "Tristana",
+		"id": 18
+	},
+	{
+		"name": "Warwick",
+		"id": 19
+	},
+	{
+		"name": "Nunu & Willump",
+		"id": 20
+	},
+	{
+		"name": "Miss Fortune",
+		"id": 21
+	},
+	{
+		"name": "Ashe",
+		"id": 22
+	},
+	{
+		"name": "Tryndamere",
+		"id": 23
+	},
+	{
+		"name": "Jax",
+		"id": 24
+	},
+	{
+		"name": "Morgana",
+		"id": 25
+	},
+	{
+		"name": "Zilean",
+		"id": 26
+	},
+	{
+		"name": "Singed",
+		"id": 27
+	},
+	{
+		"name": "Evelynn",
+		"id": 28
+	},
+	{
+		"name": "Twitch",
+		"id": 29
+	},
+	{
+		"name": "Karthus",
+		"id": 30
+	},
+	{
+		"name": "Cho'Gath",
+		"id": 31
+	},
+	{
+		"name": "Amumu",
+		"id": 32
+	},
+	{
+		"name": "Rammus",
+		"id": 33
+	},
+	{
+		"name": "Anivia",
+		"id": 34
+	},
+	{
+		"name": "Shaco",
+		"id": 35
+	},
+	{
+		"name": "Dr. Mundo",
+		"id": 36
+	},
+	{
+		"name": "Sona",
+		"id": 37
+	},
+	{
+		"name": "Kassadin",
+		"id": 38
+	},
+	{
+		"name": "Irelia",
+		"id": 39
+	},
+	{
+		"name": "Janna",
+		"id": 40
+	},
+	{
+		"name": "Gangplank",
+		"id": 41
+	},
+	{
+		"name": "Corki",
+		"id": 42
+	},
+	{
+		"name": "Karma",
+		"id": 43
+	},
+	{
+		"name": "Taric",
+		"id": 44
+	},
+	{
+		"name": "Veigar",
+		"id": 45
+	},
+	{
+		"name": "Trundle",
+		"id": 48
+	},
+	{
+		"name": "Swain",
+		"id": 50
+	},
+	{
+		"name": "Caitlyn",
+		"id": 51
+	},
+	{
+		"name": "Blitzcrank",
+		"id": 53
+	},
+	{
+		"name": "Malphite",
+		"id": 54
+	},
+	{
+		"name": "Katarina",
+		"id": 55
+	},
+	{
+		"name": "Nocturne",
+		"id": 56
+	},
+	{
+		"name": "Maokai",
+		"id": 57
+	},
+	{
+		"name": "Renekton",
+		"id": 58
+	},
+	{
+		"name": "Jarvan IV",
+		"id": 59
+	},
+	{
+		"name": "Elise",
+		"id": 60
+	},
+	{
+		"name": "Orianna",
+		"id": 61
+	},
+	{
+		"name": "Wukong",
+		"id": 62
+	},
+	{
+		"name": "Brand",
+		"id": 63
+	},
+	{
+		"name": "Lee Sin",
+		"id": 64
+	},
+	{
+		"name": "Vayne",
+		"id": 67
+	},
+	{
+		"name": "Rumble",
+		"id": 68
+	},
+	{
+		"name": "Cassiopeia",
+		"id": 69
+	},
+	{
+		"name": "Skarner",
+		"id": 72
+	},
+	{
+		"name": "Heimerdinger",
+		"id": 74
+	},
+	{
+		"name": "Nasus",
+		"id": 75
+	},
+	{
+		"name": "Nidalee",
+		"id": 76
+	},
+	{
+		"name": "Udyr",
+		"id": 77
+	},
+	{
+		"name": "Poppy",
+		"id": 78
+	},
+	{
+		"name": "Gragas",
+		"id": 79
+	},
+	{
+		"name": "Pantheon",
+		"id": 80
+	},
+	{
+		"name": "Ezreal",
+		"id": 81
+	},
+	{
+		"name": "Mordekaiser",
+		"id": 82
+	},
+	{
+		"name": "Yorick",
+		"id": 83
+	},
+	{
+		"name": "Akali",
+		"id": 84
+	},
+	{
+		"name": "Kennen",
+		"id": 85
+	},
+	{
+		"name": "Garen",
+		"id": 86
+	},
+	{
+		"name": "Leona",
+		"id": 89
+	},
+	{
+		"name": "Malzahar",
+		"id": 90
+	},
+	{
+		"name": "Talon",
+		"id": 91
+	},
+	{
+		"name": "Riven",
+		"id": 92
+	},
+	{
+		"name": "Kog'Maw",
+		"id": 96
+	},
+	{
+		"name": "Shen",
+		"id": 98
+	},
+	{
+		"name": "Lux",
+		"id": 99
+	},
+	{
+		"name": "Xerath",
+		"id": 101
+	},
+	{
+		"name": "Shyvana",
+		"id": 102
+	},
+	{
+		"name": "Ahri",
+		"id": 103
+	},
+	{
+		"name": "Graves",
+		"id": 104
+	},
+	{
+		"name": "Fizz",
+		"id": 105
+	},
+	{
+		"name": "Volibear",
+		"id": 106
+	},
+	{
+		"name": "Rengar",
+		"id": 107
+	},
+	{
+		"name": "Varus",
+		"id": 110
+	},
+	{
+		"name": "Nautilus",
+		"id": 111
+	},
+	{
+		"name": "Viktor",
+		"id": 112
+	},
+	{
+		"name": "Sejuani",
+		"id": 113
+	},
+	{
+		"name": "Fiora",
+		"id": 114
+	},
+	{
+		"name": "Ziggs",
+		"id": 115
+	},
+	{
+		"name": "Lulu",
+		"id": 117
+	},
+	{
+		"name": "Draven",
+		"id": 119
+	},
+	{
+		"name": "Hecarim",
+		"id": 120
+	},
+	{
+		"name": "Kha'Zix",
+		"id": 121
+	},
+	{
+		"name": "Darius",
+		"id": 122
+	},
+	{
+		"name": "Jayce",
+		"id": 126
+	},
+	{
+		"name": "Lissandra",
+		"id": 127
+	},
+	{
+		"name": "Diana",
+		"id": 131
+	},
+	{
+		"name": "Quinn",
+		"id": 133
+	},
+	{
+		"name": "Syndra",
+		"id": 134
+	},
+	{
+		"name": "Aurelion Sol",
+		"id": 136
+	},
+	{
+		"name": "Kayn",
+		"id": 141
+	},
+	{
+		"name": "Zoe",
+		"id": 142
+	},
+	{
+		"name": "Zyra",
+		"id": 143
+	},
+	{
+		"name": "Kai'Sa",
+		"id": 145
+	},
+	{
+		"name": "Seraphine",
+		"id": 147
+	},
+	{
+		"name": "Gnar",
+		"id": 150
+	},
+	{
+		"name": "Zac",
+		"id": 154
+	},
+	{
+		"name": "Yasuo",
+		"id": 157
+	},
+	{
+		"name": "Vel'Koz",
+		"id": 161
+	},
+	{
+		"name": "Taliyah",
+		"id": 163
+	},
+	{
+		"name": "Camille",
+		"id": 164
+	},
+	{
+		"name": "Akshan",
+		"id": 166
+	},
+	{
+		"name": "Bel'Veth",
+		"id": 200
+	},
+	{
+		"name": "Braum",
+		"id": 201
+	},
+	{
+		"name": "Jhin",
+		"id": 202
+	},
+	{
+		"name": "Kindred",
+		"id": 203
+	},
+	{
+		"name": "Zeri",
+		"id": 221
+	},
+	{
+		"name": "Jinx",
+		"id": 222
+	},
+	{
+		"name": "Tahm Kench",
+		"id": 223
+	},
+	{
+		"name": "Briar",
+		"id": 233
+	},
+	{
+		"name": "Viego",
+		"id": 234
+	},
+	{
+		"name": "Senna",
+		"id": 235
+	},
+	{
+		"name": "Lucian",
+		"id": 236
+	},
+	{
+		"name": "Zed",
+		"id": 238
+	},
+	{
+		"name": "Kled",
+		"id": 240
+	},
+	{
+		"name": "Ekko",
+		"id": 245
+	},
+	{
+		"name": "Qiyana",
+		"id": 246
+	},
+	{
+		"name": "Vi",
+		"id": 254
+	},
+	{
+		"name": "Aatrox",
+		"id": 266
+	},
+	{
+		"name": "Nami",
+		"id": 267
+	},
+	{
+		"name": "Azir",
+		"id": 268
+	},
+	{
+		"name": "Yuumi",
+		"id": 350
+	},
+	{
+		"name": "Samira",
+		"id": 360
+	},
+	{
+		"name": "Thresh",
+		"id": 412
+	},
+	{
+		"name": "Illaoi",
+		"id": 420
+	},
+	{
+		"name": "Rek'Sai",
+		"id": 421
+	},
+	{
+		"name": "Ivern",
+		"id": 427
+	},
+	{
+		"name": "Kalista",
+		"id": 429
+	},
+	{
+		"name": "Bard",
+		"id": 432
+	},
+	{
+		"name": "Rakan",
+		"id": 497
+	},
+	{
+		"name": "Xayah",
+		"id": 498
+	},
+	{
+		"name": "Ornn",
+		"id": 516
+	},
+	{
+		"name": "Sylas",
+		"id": 517
+	},
+	{
+		"name": "Neeko",
+		"id": 518
+	},
+	{
+		"name": "Aphelios",
+		"id": 523
+	},
+	{
+		"name": "Rell",
+		"id": 526
+	},
+	{
+		"name": "Pyke",
+		"id": 555
+	},
+	{
+		"name": "Vex",
+		"id": 711
+	},
+	{
+		"name": "Yone",
+		"id": 777
+	},
+	{
+		"name": "Sett",
+		"id": 875
+	},
+	{
+		"name": "Lillia",
+		"id": 876
+	},
+	{
+		"name": "Gwen",
+		"id": 887
+	},
+	{
+		"name": "Renata Glasc",
+		"id": 888
+	},
+	{
+		"name": "Nilah",
+		"id": 895
+	},
+	{
+		"name": "K'Sante",
+		"id": 897
+	},
+	{
+		"name": "Milio",
+		"id": 902
+	},
+	{
+		"name": "Naafiri",
+		"id": 950
+	}
+]
