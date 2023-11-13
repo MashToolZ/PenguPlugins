f5b41e4e3df767ef4e4ca755af55214c8990183c
Sakurasou <realmashiroshiina@gmail.com>
2023-11-13 10:20:17 +0100

    Added Toggle button to AutoHonor

--- src/AutoHonor/index.ts
+++ src/AutoHonor/index.ts
@@ -1,15 +1,44 @@
 import { MTZPlugin } from "@Classes"
-import { waitUntil } from "@Utils"
+import { Toggle, ToggleOptions } from "@Helpers"
+import { select, waitUntil } from "@Utils"
 
 new class extends MTZPlugin {
 
+	private button: HTMLElement
+	private options: ToggleOptions
+
 	constructor() {
 		super("AutoHonor", "1.0.0", "Sakurasou")
+
+		this.options = {
+			parent: ".open-party-toggle",
+			name: this.name,
+			className: "auto-honor",
+			tooltip: "Automatically honor a random teammate"
+		}
+
+		this.addCSS("https://cdn.mashtoolz.xyz/lolclient/css/AutoHonor.css")
+	}
+
+	override onScreen(screen: string) {
+		switch (screen) {
+			case "PARTIES": {
+				waitUntil(() => select(this.options.parent), 1000, false)
+					.then(() => {
+						if (!select(`#${this.options.name}`))
+							this.button = new Toggle(this.options)
+					})
+					.catch((err) => console.error(err))
+				break
+			}
+		}
 	}
 
 	override onPhase(phase: string, lastPhase: string): void {
 		switch (phase) {
 			case "PREENDOFGAME": {
+				if (!(DataStore.get(`MTZ.${this.name}`) || false)) return
+
 				waitUntil(() => {
 					const votes = [...document.querySelectorAll(".prompted-voting-honor-category-selector")]
 					return votes.length > 0 ? votes : null
