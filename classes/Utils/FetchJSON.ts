1757cf9ee2d0f5c8f00cc2630568be39a1bdf588
Sakurasou <realmashiroshiina@gmail.com>
2023-11-20 14:00:40 +0100

    cleanup & check for json

--- classes/Utils/FetchJSON.ts
+++ classes/Utils/FetchJSON.ts
@@ -1,21 +1,23 @@
+const defaultOptions: RequestInit = {
+	method: "GET",
+	headers: {
+		"Accept": "application/json",
+		"Content-Type": "application/json"
+	}
+}
+
 /**
  * Fetches JSON data from the specified URL using the provided options.
  * @param url - The URL to fetch the JSON data from.
  * @param options - The options to use for the fetch request.
  * @returns A Promise that resolves to the JSON data.
  */
-export async function FetchJSON(url: string, options?: RequestInit): Promise<any> {
+export async function FetchJSON(url: string, options: RequestInit = defaultOptions): Promise<any> {
 	try {
-		const data = await fetch(url, options || {
-			method: "GET",
-			headers: {
-				"Accept": "application/json",
-				"Content-Type": "application/json"
-			}
-		})
-		return await data.json()
+		const response = await fetch(url, options);
+		const contentType = response.headers.get("content-type")
+		return contentType?.includes("application/json") ? await response.json() : Promise.resolve(null)
 	} catch (e) {
-		console.error(e)
 		return Promise.resolve(null)
 	}
 }
