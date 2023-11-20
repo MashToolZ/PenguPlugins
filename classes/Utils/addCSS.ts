e4b8c7d4f16c42990742e9dfb07eaaa10cff526d
Sakurasou <realmashiroshiina@gmail.com>
2023-11-20 14:00:19 +0100

    Formatting

--- classes/Utils/addCSS.ts
+++ classes/Utils/addCSS.ts
@@ -6,18 +6,17 @@ import { waitUntil } from "@Utils"
  * @param remote - Whether the CSS file is remote or not. Default is true.
  */
 export function addCSS(url: string, remote = true) {
-	waitUntil(() => document.body)
-		.then(() => {
-			if (remote) {
-				const link = document.createElement("link")
-				link.setAttribute("rel", "stylesheet")
-				link.setAttribute("href", url)
-				document.body.appendChild(link)
-			} else {
-				const style = document.createElement("style")
-				style.setAttribute("type", "text/css")
-				style.innerHTML = url
-				document.body.appendChild(style)
-			}
-		})
+	waitUntil(() => document.body).then(() => {
+		if (remote) {
+			const link = document.createElement("link")
+			link.setAttribute("rel", "stylesheet")
+			link.setAttribute("href", url)
+			document.body.appendChild(link)
+		} else {
+			const style = document.createElement("style")
+			style.setAttribute("type", "text/css")
+			style.innerHTML = url
+			document.body.appendChild(style)
+		}
+	})
 }
